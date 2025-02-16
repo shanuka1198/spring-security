cad2218cbf7f4ebbec14ae32acb5214f62ae104f
shanuka1198 <shanukaimantha0@gmail.com>
2025-02-16 08:54:22 +0530

    implemented SecurityFilterChain in WebConfig

---
+++ src/main/java/com/shanuka_spring/shanuka_spring_3/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.shanuka_spring.shanuka_spring_3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(c->c.disable())
+                .authorizeHttpRequests(req->req
+                        .requestMatchers("/api/v1/auth/login","/api/v1/auth/register")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+                        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
